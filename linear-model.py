57da3ee75d22ba4f1f344d102b5e08155ba377fd
himanshu aswal <haswal403@gmail.com>
2018-11-10 16:44:38 +0530

    fit the model

--- linear-model.py
+++ linear-model.py
@@ -2,3 +2,4 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 bmi_life_data = pd.read_csv("bmi_and_life_expectancy.csv")
 bmi_life_model = LinearRegression()
+bmi_life_model.fit(bmi_life_data[['BMI']], bmi_life_data[['Life expectancy']])
